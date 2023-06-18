99230b5d8d1b52b2898277ac84fa8d267fff8b1f
skygg15 <132255966+skygg15@users.noreply.github.com>
2023-06-18 20:24:53 +0300

    Add files via upload

--- tests/test_pokemon.py
+++ tests/test_pokemon.py
@@ -2,11 +2,17 @@ import requests
 import pytest
 
 host = 'https://pokemonbattle.me:9104'
-token = '23f40f63216a35d685ea6783a86e444b'
+token = 'b177f67d1f3265acc2dca5c94d856485'
 
-def test_part_of_answer():
-    answer = requests.get(f'{host}/trainers'),
+def test_status_code():
+    answer = requests.get(f'{host}/trainers', json =
+    {
+        "name": "Бульбазавр",
+         "photo": "https://dolnikov.ru/pokemons/albums/001.png"
+          }, headers = {"Content-Type" : "application/json", 'trainer_token' : 'token'})
+    assert answer.status_code == 200
 
 
-def test_part_of_answer():
-  answer = requests.get(f'{host}/trainers', params = {'trainer_id' : 4710})
+    def test_status_code():
+      answer_body = requests.get(f'{host}/trainers', params = {'trainer_id' : 4807})
+      assert answer_body.json()['trainer_name'] == Assna
